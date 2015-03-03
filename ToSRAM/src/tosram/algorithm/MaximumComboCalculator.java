c3261e4f4c815271d746b9cda4d390351cb24b1b
johnchen902 <johnchen902@gmail.com>
2015-03-04 02:19:13 +0800

    Maximum combo calculation may have more use.

---
+++ ToSRAM/src/tosram/algorithm/MaximumComboCalculator.java
@@ -0,0 +1,59 @@
+package tosram.algorithm;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import tosram.RuneMap;
+import tosram.RuneStone;
+
+/**
+ * A utility computing maximum combo of a <code>RuneMap</code>
+ * 
+ * @author johnchen902
+ */
+public class MaximumComboCalculator {
+	private MaximumComboCalculator() {
+	}
+
+	/**
+	 * Get the maximum possible combo for the specified <code>RuneMap</code>
+	 * 
+	 * @param map
+	 *            the specified <code>RuneMap</code>
+	 * @return the maximum possible combo
+	 */
+	public static int getMaxCombo(RuneMap map) {
+		return map.stream().filter(s -> s != null && s != RuneStone.UNKNOWN)
+				.collect(groupingBy(identity(), counting())).values().stream()
+				.mapToInt(MaximumComboCalculator::getMaxComboSingleElement)
+				.sum();
+	}
+
+	private static int getMaxComboSingleElement(long count) {
+		return getMaxComboSingleElement((int) count);
+	}
+
+	private static int getMaxComboSingleElement(int count) {
+		if (count <= 20)
+			return count / 3;
+		switch (count) {
+		case 21:
+		case 22:
+			return 5;
+		case 23:
+			return 4;
+		case 24:
+		case 25:
+			return 3;
+		case 26:
+			return 2;
+		case 27:
+		case 28:
+		case 29:
+		case 30:
+			return 1;
+		default:
+			return 0;
+		}
+	}
+}
--- ToSRAM/src/tosram/algorithm/idastar/ComboHeuristicCostEstimater.java
+++ ToSRAM/src/tosram/algorithm/idastar/ComboHeuristicCostEstimater.java
@@ -1,15 +1,11 @@
 package tosram.algorithm.idastar;
 
-import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.groupingBy;
-
 import java.util.List;
 
 import tosram.MutableRuneMap;
 import tosram.RuneMap;
-import tosram.RuneStone;
 import tosram.algorithm.ComboCountingAlgorithm.Combo;
+import tosram.algorithm.MaximumComboCalculator;
 
 /**
  * The h-function considering numbers of combo.
@@ -31,7 +27,7 @@ public class ComboHeuristicCostEstimater implements HeuristicCostEstimater {
 
 	@Override
 	public void setSourceState(RuneMap map) {
-		maxCombo = getMaxCombo(map);
+		maxCombo = MaximumComboCalculator.getMaxCombo(map);
 	}
 
 	@Override
@@ -42,44 +38,5 @@ public class ComboHeuristicCostEstimater implements HeuristicCostEstimater {
 	@Override
 	public String describe(MutableRuneMap map, List<Combo> combo) {
 		return String.format("%d/%d Combo", combo.size(), maxCombo);
-	}
-
-	private static int getMaxCombo(RuneMap map) {
-		return map
-				.stream()
-				.filter(s -> s != null && s != RuneStone.UNKNOWN)
-				.collect(groupingBy(identity(), counting()))
-				.values()
-				.stream()
-				.mapToInt(ComboHeuristicCostEstimater::getMaxComboSingleElement)
-				.sum();
-	}
-
-	private static int getMaxComboSingleElement(long count) {
-		return getMaxComboSingleElement((int) count);
-	}
-
-	private static int getMaxComboSingleElement(int count) {
-		if (count <= 20)
-			return count / 3;
-		switch (count) {
-		case 21:
-		case 22:
-			return 5;
-		case 23:
-			return 4;
-		case 24:
-		case 25:
-			return 3;
-		case 26:
-			return 2;
-		case 27:
-		case 28:
-		case 29:
-		case 30:
-			return 1;
-		default:
-			return 0;
-		}
 	}
 }
