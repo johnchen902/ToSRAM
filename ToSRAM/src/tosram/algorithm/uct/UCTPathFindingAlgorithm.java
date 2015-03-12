7d7073865ea1b928e3d86fb9c898c1277973644c
johnchen902 <johnchen902@gmail.com>
2015-03-12 17:04:29 +0800

    Fix the UCT algorithm.

--- ToSRAM/src/tosram/algorithm/uct/UCTPathFindingAlgorithm.java
+++ ToSRAM/src/tosram/algorithm/uct/UCTPathFindingAlgorithm.java
@@ -2,7 +2,6 @@ package tosram.algorithm.uct;
 
 import java.awt.Point;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.List;
@@ -19,23 +18,28 @@ import tosram.algorithm.ComboCountingAlgorithm;
 import tosram.algorithm.MaximumComboCalculator;
 import tosram.algorithm.PathConstrain;
 
-// TODO document
-// Upper Confidence bound applied in Tree
+/**
+ * MonteCarlo Tree Search with Upper Confidence Bound applied in Tree.
+ * 
+ * @author johnchen902
+ */
 public class UCTPathFindingAlgorithm extends AbstractPathFindingAlgorithm {
+
+	private static final int VALUE_PER_COMBO = 10;
+	private int maximumPossibleCombo;
+	private int highestValue = 0;
+	private int highestPossibleValue = 0;
 
 	public UCTPathFindingAlgorithm(ComboCountingAlgorithm comboCounter,
 			PathConstrain constrain) {
 		super(comboCounter, constrain);
 	}
 
-	private int highestValue = 0;
-	private int highestPossibleValue = 0;
-
 	@Override
 	protected void findPath(RuneMap initialMap) {
-		int maxCombo = MaximumComboCalculator.getMaxCombo(initialMap);
+		maximumPossibleCombo = MaximumComboCalculator.getMaxCombo(initialMap);
 		highestValue = 0;
-		highestPossibleValue = maxCombo * 20;
+		highestPossibleValue = maximumPossibleCombo * VALUE_PER_COMBO;
 		try {
 			MutableRuneMap m = initialMap.toMutable();
 			Root root = new Root(m);
@@ -44,16 +48,19 @@ public class UCTPathFindingAlgorithm extends AbstractPathFindingAlgorithm {
 		} catch (OutOfMemoryError e) {
 			e.printStackTrace();
 		}
+		System.gc();
 	}
 
 	private int evaluate(MutableRuneMap map, List<Direction> directions) {
-		int v = comboCounter.countCombo(map).size() * 20 - directions.size();
+		int combo = comboCounter.countCombo(map).size();
+		int v = combo * VALUE_PER_COMBO - directions.size();
 		return Math.max(v, 0);
 	}
 
 	private String message(MutableRuneMap map, List<Direction> directions) {
-		return comboCounter.countCombo(map).size() + " Combo "
-				+ directions.size() + " Move";
+		int combo = comboCounter.countCombo(map).size();
+		return String.format("%d/%d Combo %d Move", combo,
+				maximumPossibleCombo, directions.size());
 	}
 
 	private class Root {
@@ -61,16 +68,11 @@ public class UCTPathFindingAlgorithm extends AbstractPathFindingAlgorithm {
 		private Map<Point, Node> children;
 
 		public Root(MutableRuneMap map) {
-			size = 1;
 			children = new HashMap<>();
 			for (int x = 0; x < map.getWidth(); x++)
 				for (int y = 0; y < map.getHeight(); y++)
-					if (constrain.canStart(x, y, map)) {
-						Point p = new Point(x, y);
-						Node n = new Node(map, x, y, Collections.emptyList());
-						children.put(p, n);
-						size += n.getSize();
-					}
+					if (constrain.canStart(x, y, map))
+						children.put(new Point(x, y), new Node());
 		}
 
 		public void select(MutableRuneMap map) {
@@ -78,50 +80,37 @@ public class UCTPathFindingAlgorithm extends AbstractPathFindingAlgorithm {
 				return;
 			Entry<Point, Node> e = selectChild();
 			Point p = e.getKey();
-			Node n = e.getValue();
-			size -= n.getSize();
-			n.select(map, p.x, p.y, new ArrayList<>(), p.x, p.y);
-			size += n.getSize();
+			e.getValue().select(map, p.x, p.y, new ArrayList<>(), p.x, p.y);
+			size++;
 		}
 
 		private Entry<Point, Node> selectChild() {
-			double rand;
-			{
-				double wsum = 0;
-				for (Node n : children.values())
-					wsum += n.weight(size - 1);
-				rand = Math.random() * wsum;
+			double highest = 0;
+			Entry<Point, Node> highestE = null;
+			for (Entry<Point, Node> e : children.entrySet()) {
+				double w = e.getValue().weight(size);
+				if (w > highest || highestE == null) {
+					highest = w;
+					highestE = e;
+				}
 			}
-			double wsum = 0;
-			for (Entry<Point, Node> e : children.entrySet())
-				if ((wsum += e.getValue().weight(size - 1)) > rand)
-					return e;
-			throw new AssertionError();
+			return highestE;
 		}
 	}
 
 	private class Node {
 		private int size;
-		private int value;
+		private int value = Integer.MIN_VALUE;
 		private EnumMap<Direction, Node> children;
-
-		public Node(MutableRuneMap map, int startX, int startY,
-				List<Direction> directions) {
-			size = 1;
-			value = evaluate(map, directions);
-			if (!directions.isEmpty() && value > highestValue) {
-				highestValue = value;
-				result(new Path(new Point(startX, startY), directions),
-						message(map, directions));
-			}
-		}
 
 		public void select(MutableRuneMap map, int startX, int startY,
 				List<Direction> directions, int x1, int y1) {
 			if (children == null) {
+				evaluate(map, startX, startY, directions);
 				expand(map, startX, startY, directions, x1, y1);
 				return;
 			}
+			size++;
 			if (children.isEmpty())
 				return;
 			Entry<Direction, Node> e = selectChild();
@@ -135,13 +124,22 @@ public class UCTPathFindingAlgorithm extends AbstractPathFindingAlgorithm {
 			map.setRuneStone(x2, y2, stone1);
 			directions.add(d);
 
-			size -= n.getSize();
 			n.select(map, startX, startY, directions, x2, y2);
-			size += n.getSize();
+			value = Math.max(value, n.value);
 
 			map.setRuneStone(x1, y1, stone1);
 			map.setRuneStone(x2, y2, stone2);
 			directions.remove(directions.size() - 1);
+		}
+
+		private void evaluate(MutableRuneMap map, int startX, int startY,
+				List<Direction> directions) {
+			value = UCTPathFindingAlgorithm.this.evaluate(map, directions);
+			if (!directions.isEmpty() && value > highestValue) {
+				highestValue = value;
+				result(new Path(new Point(startX, startY), directions),
+						message(map, directions));
+			}
 		}
 
 		private void expand(MutableRuneMap map, int startX, int startY,
@@ -157,46 +155,26 @@ public class UCTPathFindingAlgorithm extends AbstractPathFindingAlgorithm {
 				if (!constrain.canMove(startX, startY, directions, d, x2, y2,
 						map))
 					continue;
-
-				RuneStone stone1 = map.getRuneStone(x1, y1);
-				RuneStone stone2 = map.getRuneStone(x2, y2);
-				map.setRuneStone(x1, y1, stone2);
-				map.setRuneStone(x2, y2, stone1);
-				directions.add(d);
-
-				Node n = new Node(map, startX, startY, directions);
-				children.put(d, n);
-				size += n.getSize();
-
-				map.setRuneStone(x1, y1, stone1);
-				map.setRuneStone(x2, y2, stone2);
-				directions.remove(directions.size() - 1);
+				children.put(d, new Node());
 			}
 		}
 
 		private Entry<Direction, Node> selectChild() {
-			if (children.size() == 1)
-				return children.entrySet().iterator().next();
-
-			double rand;
-			{
-				double wsum = 0;
-				for (Node n : children.values())
-					wsum += n.weight(size - 1);
-				rand = Math.random() * wsum;
+			double highest = 0;
+			Entry<Direction, Node> highestE = null;
+			for (Entry<Direction, Node> e : children.entrySet()) {
+				double w = e.getValue().weight(size);
+				if (w > highest || highestE == null) {
+					highest = w;
+					highestE = e;
+				}
 			}
-			double wsum = 0;
-			for (Entry<Direction, Node> e : children.entrySet())
-				if ((wsum += e.getValue().weight(size - 1)) > rand)
-					return e;
-			throw new AssertionError();
-		}
-
-		public int getSize() {
-			return size;
+			return highestE;
 		}
 
 		public double weight(int t) {
+			if (children == null)
+				return Double.POSITIVE_INFINITY;
 			return (double) value / highestPossibleValue
 					+ Math.sqrt(2 * Math.log(t) / size);
 		}
