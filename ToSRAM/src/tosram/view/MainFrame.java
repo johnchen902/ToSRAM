5a1a6346835fdf169c32463dd045f0929ea11ff9
John Chen <johnchen902@gmail.com>
2015-07-20 11:38:11 +0800

    Clean up MainFrame a bit.

--- ToSRAM/src/tosram/view/MainFrame.java
+++ ToSRAM/src/tosram/view/MainFrame.java
@@ -2,7 +2,7 @@ package tosram.view;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.GridLayout;
+import java.awt.Rectangle;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.util.List;
@@ -21,7 +21,7 @@ import tosram.RuneMap;
 import tosram.algorithm.PathFinder;
 
 /**
- * The frame shown on the screen.
+ * The frame containing most of the user interface of this program.
  * 
  * @author johnchen902
  */
@@ -31,7 +31,6 @@ public class MainFrame extends JFrame {
 	private static final String STATUS_IDLE = "Idle";
 	private static final String STATUS_EDITTING = "Editting";
 	private static final String STATUS_COMPUTING = "Computing";
-	private static final String STATUS_NOW = "Now";
 	private static final String STATUS_ERROR = "Error";
 
 	private static final String BUTTON_EDIT = "Edit";
@@ -40,31 +39,22 @@ public class MainFrame extends JFrame {
 	private static final String BUTTON_STOP = "Stop";
 	private static final String BUTTON_SETTINGS = "Settings";
 
-	private JPanel pnMain;
-	private JLabel lbStatus; // general states and computing milestones
-	private PathPanel pnPath; // path drawn above tbStones
+	private JLabel lbStatus;
 	private RuneMapTable tbStones;
+	private PathPanel pnPath;
 	private Settings settings;
-
-	private JPanel buttonsPanel;
 	private JButton btEdit;
 	private JButton btCompute;
 	private JButton btStop;
 	private JButton btSettings;
 
 	private RuneMap runeMap;
-	private PathFinder algorithm;
+	private PathFinder pathFinder;
 
 	public MainFrame() {
 		super("Tower of Savior Runestone Auto Mover");
 
-		initUI();
-
-		settings = new Settings();
-	}
-
-	private void initUI() {
-		initMainPanel();
+		JPanel pnMain = createMainPanel();
 
 		getContentPane().add(pnMain);
 		setSize(400, 400);
@@ -72,63 +62,49 @@ public class MainFrame extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationByPlatform(true);
 		setVisible(true);
+
+		settings = new Settings();
 	}
 
-	private void initMainPanel() {
-		pnMain = new JPanel();
-		pnMain.setLayout(new BorderLayout());
+	private JPanel createMainPanel() {
+		JPanel pnMain = new JPanel(new BorderLayout());
 
 		{
-			JPanel pnNorth = new JPanel();
-			pnMain.add(pnNorth, BorderLayout.NORTH);
-			pnNorth.setLayout(new GridLayout(0, 1, 0, 0));
-
 			lbStatus = new JLabel();
-			pnNorth.add(lbStatus);
+			pnMain.add(lbStatus, BorderLayout.NORTH);
 			lbStatus.setText(STATUS_IDLE);
 			lbStatus.setFont(lbStatus.getFont().deriveFont(24f));
 			lbStatus.setHorizontalAlignment(JLabel.CENTER);
 		}
 		{
-			JPanel pnCenter = new JPanel();
-			pnMain.add(pnCenter, BorderLayout.CENTER);
-			pnCenter.setLayout(new GridLayout(1, 0, 5, 0));
+			JLayeredPane layeredPane = new JLayeredPane();
+			pnMain.add(layeredPane, BorderLayout.CENTER);
+			layeredPane.addComponentListener(new ComponentAdapter() {
+				@Override
+				public void componentResized(ComponentEvent e) {
+					Component c = e.getComponent();
+					pnPath.setBounds(new Rectangle(c.getSize()));
+					pnPath.setCellSize(c.getWidth() / 6, c.getHeight() / 5);
+					tbStones.setBounds(new Rectangle(c.getSize()));
+					c.validate();
+				}
+			});
 
-			{
-				JLayeredPane layeredPane = new JLayeredPane();
-				pnCenter.add(layeredPane);
-				layeredPane.addComponentListener(new ComponentAdapter() {
-					@Override
-					public void componentResized(ComponentEvent e) {
-						Component c = e.getComponent();
-						pnPath.setBounds(c.getBounds());
-						pnPath.setCellSize(c.getWidth() / 6, c.getHeight() / 5);
-						tbStones.setBounds(c.getBounds());
-						c.validate();
-					}
-				});
+			pnPath = new PathPanel();
+			layeredPane.add(pnPath, Integer.valueOf(2));
 
-				pnPath = new PathPanel();
-				layeredPane.add(pnPath, Integer.valueOf(2));
-
-				tbStones = new RuneMapTable();
-				layeredPane.add(tbStones, Integer.valueOf(1));
-				tbStones.setFont(tbStones.getFont().deriveFont(24f));
-				runeMap = tbStones.getRuneMap();
-			}
+			tbStones = new RuneMapTable();
+			layeredPane.add(tbStones, Integer.valueOf(1));
+			tbStones.setFont(tbStones.getFont().deriveFont(24f));
+			runeMap = tbStones.getRuneMap();
 		}
 		{
-			buttonsPanel = new JPanel();
+			JPanel buttonsPanel = new JPanel();
 			pnMain.add(buttonsPanel, BorderLayout.SOUTH);
 
 			btEdit = new JButton(BUTTON_EDIT);
 			buttonsPanel.add(btEdit);
-			btEdit.addActionListener(e -> {
-				if (btEdit.getText().equals(BUTTON_EDIT))
-					startEditing();
-				else
-					finishEditing();
-			});
+			btEdit.addActionListener(e -> toggleEditing());
 
 			btCompute = new JButton(BUTTON_COMPUTE);
 			buttonsPanel.add(btCompute);
@@ -143,84 +119,97 @@ public class MainFrame extends JFrame {
 			buttonsPanel.add(btSettings);
 			btSettings.addActionListener(e -> showSettings());
 		}
+		return pnMain;
+	}
+
+	private void toggleEnabled(JButton... buttons) {
+		for (JButton button : buttons)
+			button.setEnabled(!button.isEnabled());
+	}
+
+	private void toggleEditing() {
+		if (btEdit.getText().equals(BUTTON_EDIT))
+			startEditing();
+		else
+			finishEditing();
 	}
 
 	private void startEditing() {
+		toggleEnabled(btCompute, btSettings);
+
+		pnPath.setPath(null);
+		tbStones.setRuneMap(runeMap);
 		btEdit.setText(BUTTON_FINISH);
 		tbStones.setRenderer(RuneButton.factory(tbStones));
 		lbStatus.setText(STATUS_EDITTING);
-		btCompute.setEnabled(false);
-		pnPath.setPath(null);
-		tbStones.setRuneMap(runeMap);
-		btSettings.setEnabled(false);
 	}
 
 	private void finishEditing() {
+		toggleEnabled(btCompute, btSettings);
+
 		btEdit.setText(BUTTON_EDIT);
 		tbStones.setRenderer(RuneLabel.factory());
 		lbStatus.setText(STATUS_IDLE);
-		btCompute.setEnabled(true);
 		runeMap = tbStones.getRuneMap();
-		btSettings.setEnabled(true);
 	}
 
 	private void startComputing() {
-		lbStatus.setText(STATUS_COMPUTING);
-		btEdit.setEnabled(false);
-		btCompute.setEnabled(false);
-		btStop.setEnabled(true);
+		toggleEnabled(btEdit, btCompute, btStop, btSettings);
+
 		pnPath.setPath(null);
-		algorithm = settings.getAlgorithm();
-		btSettings.setEnabled(false);
-		new SwingWorker<Void, Object[]>() {
-			@Override
-			protected Void doInBackground() {
-				algorithm.findPath(runeMap, (p, s) -> publish(new Object[] { p,
-						s }));
-				return null;
-			}
-
-			@Override
-			protected void process(List<Object[]> chunks) {
-				Object[] pair = chunks.get(chunks.size() - 1);
-				Path path = (Path) pair[0];
-				pnPath.setPath(path);
-				lbStatus.setText(STATUS_NOW + ": " + pair[1]);
-				tbStones.setRuneMap(Path.follow(runeMap, path));
-				repaint();
-			}
-
-			@Override
-			protected void done() {
-				stopComputing();
-				try {
-					get();
-				} catch (ExecutionException e) {
-					e.printStackTrace();
-					lbStatus.setText(STATUS_ERROR + ": " + e.getCause());
-				} catch (InterruptedException e) {
-				}
-			}
-		}.execute();
+		lbStatus.setText(STATUS_COMPUTING);
+		pathFinder = settings.getAlgorithm();
+		new ComputationWorker().execute();
 	}
 
 	private void stopComputing() {
 		if (btStop.isEnabled()) {
-			btEdit.setEnabled(true);
-			btCompute.setEnabled(true);
-			btStop.setEnabled(false);
-			algorithm.stop();
-			btSettings.setEnabled(true);
+			toggleEnabled(btEdit, btCompute, btStop, btSettings);
+
+			pathFinder.stop();
 		}
 	}
 
 	private void showSettings() {
-		int result = JOptionPane.showConfirmDialog(this,
-				settings.getEditorPanel(), "Settings",
+		Object editor = settings.getEditorPanel();
+		int result = JOptionPane.showConfirmDialog(this, editor, "Settings",
 				JOptionPane.OK_CANCEL_OPTION);
 		if (result == JOptionPane.OK_OPTION)
 			settings.commit();
 		else
 			settings.cancel();
+	}
+
+	private class ComputationWorker extends SwingWorker<Void, Object[]> {
+		@Override
+		protected Void doInBackground() {
+			pathFinder.findPath(runeMap,
+					(path, message) -> publish(new Object[] { path, message }));
+			return null;
+		}
+
+		@Override
+		protected void process(List<Object[]> chunks) {
+			Object[] pair = chunks.get(chunks.size() - 1);
+			Path path = (Path) pair[0];
+			String message = (String) pair[1];
+			pnPath.setPath(path);
+			lbStatus.setText(message);
+			tbStones.setRuneMap(Path.follow(runeMap, path));
+			repaint();
+		}
+
+		@Override
+		protected void done() {
+			stopComputing();
+			try {
+				get();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+				lbStatus.setText(STATUS_ERROR + ": " + e.getCause());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
